930462aaee8f6465e8575c69d729e0abc8c839f2
Jackson Lin <think.jackson@gmail.com>
2014-01-30 09:00:40 -0500

    sn copied

---
+++ client/compatibility/sn/js/profile.js
@@ -0,0 +1,37 @@
+  jQuery(document).ready(function() {
+    profile = new Object();
+    profile.largeimage = Array;
+    profile.smallimage = Array;
+    profile.h = jQuery(window).height();
+    profile.w = jQuery(window).width();
+    profile.navh = jQuery('#container .navbar-static-top').height()
+    //jQuery('.fullwidthimage').css({ paddingTop: profile.navh });
+    profile.largeHeaders = jQuery(window).height() - jQuery(window).height()*.1;
+    if (profile.largeHeaders < 450 ){ profile.largeHeaders = 450; };
+    //jQuery('#wrap').css({ marginTop: jQuery('#container > header').height() });
+    jQuery('.fullwidthimage').css({ height: profile.largeHeaders/1.45 });
+    jQuery('.fullwidthimage .title > div').css({ height: profile.largeHeaders/1.45 });
+        jQuery('.home .fullwidthimage.home-logo').css({ height: profile.largeHeaders + jQuery('header .nav-main').height(), backgroundPosition: 'center center' /*, backgroundSize: '100% auto'*/ });
+    //jQuery('.single .fullwidthimage.post').css({ height: profile.largeHeaders*.95 + jQuery('header .nav-main').height(), backgroundPosition: 'center center', backgroundSize: '100%' });
+    
+    
+    jQuery('.home .fullwidthimage.home-logo .title > div').css({ height: profile.largeHeaders + jQuery('header .nav-main').height() });
+    //jQuery('.single .fullwidthimage.post .title div').css({ height: profile.largeHeaders*.95 + jQuery('header .nav-main').height() });
+    
+    /*jQuery(window).scroll(function () {
+      var fromTop = 0;
+      var scrollTop = jQuery(window).scrollTop()
+      jQuery('.fullwidthimage').each(function() {
+        fromTop = jQuery(this).offset().top + profile.largeHeaders - scrollTop;
+        //console.log(fromTop);
+        if (fromTop > 0 && fromTop < profile.largeHeaders ){
+          fromTop = (fromTop - profile.largeHeaders)/2;
+          fromTop = '0px ' + fromTop + 'px';
+          //console.log(fromTop);
+          jQuery(this).css({
+              backgroundPosition: fromTop
+            });
+        }
+      });
+    });*/
+  });
